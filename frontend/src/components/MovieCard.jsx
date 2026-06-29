e2c898703c2f26c0d72f2bc5beabd7bb533a2a67
VuongDuongg <funnyvuong@gmail.com>
2026-06-29 12:39:47 +0700

    favorite
    favorite

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,9 +1,20 @@
 import '../css/MovieCard.css'
+import React from 'react'
+import { useMovieContext } from '../contexts/MovieContext'
+
 
 function MovieCard({ movie }) {
 
-    function onclickFavorite() {
-        alert(`You have favorited ${movie.title}`);
+    const { addToFavorites, removeFromFavorites, isFavorite } = useMovieContext();
+    const favorite = isFavorite(movie.id);
+
+    function onclickFavorite(e) {
+        e.preventDefault();
+        if (favorite) {
+            removeFromFavorites(movie.id);
+        } else {
+            addToFavorites(movie);
+        }
     }
 
     return (
@@ -12,8 +23,16 @@ function MovieCard({ movie }) {
                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
 
                 <div className="movie-overlay">
-                    <button className="favorite-btn" onClick={onclickFavorite}>
-                        🤍
+                    <button className={`favorite-btn ${favorite ? 'active' : ''}`} onClick={onclickFavorite}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24" height="24" viewBox="0 0 24 24"
+                            fill={favorite ? "#ff4757" : "none"}
+                            stroke={favorite ? "#ff4757" : "white"}
+                            strokeWidth="2"
+                        >
+                            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+                        </svg>
                     </button>
                 </div>
             </div>
